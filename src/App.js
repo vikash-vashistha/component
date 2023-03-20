28217935d7db6b0133653e116805d3d00589bef6
Vikash Vashistha <vikashvashistha.dev@gmail.com>
2023-03-20 18:40:38 +0530

    component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  FormControl,
+  Stack,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Text,
+  Heading,
+  Box,
+  StackDivider,
+  Flex,
+  Button,
+  Input,
+  FormLabel,
+} from "@chakra-ui/react";
+import { FcBusinessman } from "react-icons/fc";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card m={50} >
+      <CardHeader >
+        <Flex align={"center"} gap={2} justify={"space-between"}>
+          <Flex align={"center"} gap={2}>
+            <Heading size="md">Customer Detials</Heading>
+            <FcBusinessman />
+          </Flex>
+          <Button colorScheme="blue" variant="outline">
+            Edit
+          </Button>
+        </Flex>
+      </CardHeader>
+
+      <CardBody>
+        <Flex gap={6}>
+          <FormControl>
+            <FormLabel>Customer Name</FormLabel>
+            <Input placeholder='Mr Fraser Lomas'/>
+            <FormLabel>Date of Birth</FormLabel>
+            <Input placeholder='14/05/1985'/>
+            <FormLabel>Email Address</FormLabel>
+            <Input placeholder='fraser.lomas@ecgglobal.com'/>
+          </FormControl>
+          <FormControl>
+            <FormLabel>Address</FormLabel>
+            <Input h={110} placeholder='123 Fake Street Preston Lancashire PR2 5YB' />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Telephone Number</FormLabel>
+            <Input placeholder='01772 111145'/>
+            <FormLabel>Alt Telephone Number</FormLabel>
+            <Input />
+          </FormControl>
+        </Flex>
+      </CardBody>
+    </Card>
   );
 }
 
